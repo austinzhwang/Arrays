b02cc3949bf81e4ac73e9aa37b1fed195726ad36
GBTC441020UR <GBTC441020UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-27 09:46:04 -0500

    fixed locker problem

--- src/Locker.java
+++ src/Locker.java
@@ -2,19 +2,18 @@ public class Locker {
 
     public static void main (String []args) {
 
+        //Creates 101 locker boolean elements; ignore locker at index 0 and start with locker 1
         boolean [] locker = new boolean [101];
 
         //Set every locker to open
         for (int i = 1; i < locker.length; i++) {
             locker[i] = true;
         }
-        //Starting with locker 2 and every 2 lockers after 2, set the locker to closed
-        for (int i = 2; i < locker.length; i+=2) {
-            locker[i] = false;
-        }
-        //Starting with locker 3 and every other 3rd locker; close the locker if open and open the locker if closed
-        for (int i = 3; i < locker.length; i++) {
-            for (int j = 1; j <locker.length; j++) {
+
+        //Starting with locker 2 and every other 2nd locker; close the locker if open and open the locker if closed
+        //Continue process with locker 3 and every nth locker after that
+        for (int i = 2; i < locker.length; i++) {
+            for (int j = i; j <locker.length; j++) {
                 if (j % i == 0) {
                     locker[j] = !locker[j];
                 }
